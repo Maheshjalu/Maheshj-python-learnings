c0945efe9118ba268de859b8464c5331ce4decaa
Maheshjalu <134523476+Maheshjalu@users.noreply.github.com>
2025-05-20 19:34:54 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,9 +1,89 @@
 arn = "arn:aws:iam:123456789012:user/johndoe"
 print(arn.split("/")[1])
-name = "Mahesh"
+name = "Mahesh is awesome"
 print(name.upper())
 print(name.lower())
 string1= "hello"
 string2= "world"
 print((string1+" " + string2))
-print (len(string1))
+print(len(string1))
+modifiedname = name.replace("Mahesh" , "MAHESH JALUMURI")
+print("modifiedname:", modifiedname)
+striptext = "    spaces are fine now   " 
+newstrip = striptext.strip()
+print("stripped text:" , newstrip)
+substring = "is"
+if substring in name:
+    print(substring, "found the string")
+
+# Float variables
+num1 = 5.0
+num2 = 2.5
+
+# Basic Arithmetic
+result1 = num1 + num2
+print("Addition:", result1)
+
+result2 = num1 - num2
+print("Subtraction:", result2)
+
+result3 = num1 * num2
+print("Multiplication:", result3)
+
+result4 = num1 / num2
+print("Division:", result4)
+
+# Rounding
+result5 = round(3.14159265359, 2)  # Rounds to 2 decimal places
+print("Rounded:", result5)
+
+# Integer variables
+num1 = 10
+num2 = 5
+
+# Integer Division
+result1 = num1 // num2
+print("Integer Division:", result1)
+
+# Modulus (Remainder)
+result2 = num1 % num2
+print("Modulus (Remainder):", result2)
+
+# Absolute Value
+result3 = abs(-7)
+print("Absolute Value:", result3)
+
+import re
+
+text = "The quick brown fox"
+pattern = r"brown"
+
+search = re.search(pattern, text)
+if search:
+    print("Pattern found:", search.group())
+else:
+    print("Pattern not found")
+
+text = "The quick brown fox"
+pattern = r"quick"
+
+match = re.match(pattern, text)
+if match:
+    print("Match found:", match.group())
+else:
+    print("No match")
+
+text = "The quick brown fox jumps over the lazy brown dog"
+pattern = r"brown"
+
+replacement = "red"
+
+new_text = re.sub(pattern, replacement, text)
+print("Modified text:", new_text)
+
+text = "apple,banana,orange,grape"
+pattern = r","
+
+split_result = re.split(pattern, text)
+print("Split result:", split_result)
+
