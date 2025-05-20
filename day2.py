7fe19508f231293d09dbb4a00e3a16d716e40fde
Maheshjalu <134523476+Maheshjalu@users.noreply.github.com>
2025-05-20 19:22:55 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -2,6 +2,7 @@ arn = "arn:aws:iam:123456789012:user/johndoe"
 print(arn.split("/")[1])
 name = "Mahesh"
 print(name.upper())
+print(name.lower())
 string1= "hello"
 string2= "world"
 print((string1+" " + string2))
