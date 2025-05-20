e0f6a24eabb6040bc8089d729ade149f39acc46e
Maheshjalu <134523476+Maheshjalu@users.noreply.github.com>
2025-05-20 19:15:59 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,8 @@
+arn = "arn:aws:iam:123456789012:user/johndoe"
+print(arn.split("/")[1])
+name = "Mahesh"
+print(name.upper())
+string1= "hello"
+string2= "world"
+print((string1+" " + string2))
+print (len(string1))
